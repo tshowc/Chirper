9830d1f0275cffeffdd27a85803f3ff1f510320f
Mary Clark <mclark6@rosemary.umw.edu>
2014-11-24 22:27:27 -0500

    what

--- Menu.java
+++ Menu.java
@@ -348,7 +348,7 @@ public class Menu
 	public void clearScreen()
 	{	
 		//clear the screen
-		System.out.print("\033[H\033[2J");
-		System.out.flush();
+		//System.out.print("\033[H\033[2J");
+		//System.out.flush();
 	}
 }
